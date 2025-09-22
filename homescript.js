cc9b201ace905939038099ea27d8c3a75abd57d0
dixter2008 <135216552+dixter2008@users.noreply.github.com>
2025-09-22 21:20:43 +0800

    Update homescript.js

--- homescript.js
+++ homescript.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/summarize", {
+            const response = await fetch("https://oralixbackend.onrender.com", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ text: textInput })
@@ -82,6 +82,7 @@ document.getElementById('generate-btn').addEventListener('click', () => {
 });
 
 });
+
 
 
 
